98d6253d7626fb9804b3cfe9ef115b18a0828986
defk0n1 <zrigua.ghassen@gmail.com>
2025-11-10 21:12:39 +0000

    init iam and dependency config v1 - still have dependency issue for beans

---
+++ iam/src/main/java/tn/supcom/cot/iam/controllers/repositories/GrantRepository.java
@@ -0,0 +1,16 @@
+package tn.supcom.cot.iam.controllers.repositories;
+
+import jakarta.data.repository.By;
+import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.Find;
+import jakarta.data.repository.Repository;
+import tn.supcom.cot.iam.entities.Grant;
+import tn.supcom.cot.iam.entities.GrantPK;
+
+
+import java.util.Optional;
+
+@Repository
+public interface GrantRepository extends CrudRepository<Grant, GrantPK> {
+
+}
